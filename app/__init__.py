e551ff11dd5a93748665d50aebb980e0a9bbb62c
GamingAstronamy <ethan-barnes@hotmail.com>
2021-12-14 22:10:13 -0600

    Logins and User Model

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from config import Config
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch = True)
+login_manager = LoginManager(app)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from app.models import User
+from flask import render_template, redirect, url_for
+from flask_login import login_required, login_user, current_user
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for(''))
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for(''))
+
+    return render_template('login.html', form=form)
