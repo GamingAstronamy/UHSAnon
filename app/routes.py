8d1f836a60e49654dbeaee36878ba32b012a9236
GamingAstronamy <ethan-barnes@hotmail.com>
2021-12-15 18:53:41 -0600

    Design

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask_login.utils import logout_user
 from app import app
 from app.forms import LoginForm
 from app.models import User
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, send_from_directory
 from flask_login import login_required, login_user, current_user
 
 @app.route('/')
@@ -23,3 +24,8 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
